56391e9068aec5a84d7a15c610a77e4aae3fae41
Khaelano Abroor Maulana <khaelanoabroor@gmail.com>
2024-07-24 22:53:33 +0700

    add new dto

--- dto/src/lib.rs
+++ dto/src/lib.rs
@@ -1,11 +1,30 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct Carrier<T> {
+    pub source: String,
+    pub data: T
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Filter {
+    pub language: String,
+    pub sort: Order,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Order {
+    Ascending,
+    Descending
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub enum PublicationStatus {
     Ongoing,
     Completed,
     Hiatus,
-    Cancelled
+    Cancelled,
+    Unknown
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -15,13 +34,21 @@ pub struct Author {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+pub struct MangaList {
+    pub page: u32,
+    pub total_page: u32,
+    pub data: Vec<Manga>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Manga {
     pub url: String,
     pub title: String,
     pub authors: Vec<Author>,
     pub original_language: String,
     pub language: String,
-    pub status: String,
+    pub description: String,
+    pub status: PublicationStatus,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
