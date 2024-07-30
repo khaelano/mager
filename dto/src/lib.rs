c817e845005464900dca51c38ae23b8968e15bd7
Khaelano Abroor Maulana <khaelanoabroor@gmail.com>
2024-07-30 17:46:46 +0700

    Added carrier module that contains struct that will be passed between client and source

--- dto/src/lib.rs
+++ dto/src/lib.rs
@@ -1,9 +1,79 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Carrier<T> {
-    pub source: String,
-    pub data: T,
+pub mod carriers {
+    // GMTP: Gajelas (or Goblok) Manga Transfer Protocol
+    // Version 0.0.0
+    //
+    // A dumb, whacky, freaky, skibidy attempt to make my own transfer protocol for transferring
+    // manga data between my applications. (i know, it's dumb)
+    // Please don't use it anywhere.
+    //
+    // Request:
+    // `
+    // GMTP-Version CRLF
+    // COMMAND [params],[],... CRLF
+    // CRLF CRLF
+    // `
+    //
+    // Response:
+    // `
+    // STATUS Reason-Phrase GMTP-Version CRLF
+    // Source-Name CRLF
+    // transferred-data
+    // CRLF CRLF
+    // `
+    //
+    // Available COMMAND:
+    // - SEARCH -> This command will ask server to search a manga (params: ["Manga Keyword"],[Filter])
+    // - CHAPTERS -> This command will ask server to fetch a manga's chapter list (params: ["Manga's URL"],[Filter])
+    // - PAGES -> This command will ask server to fetch a chapter's URL for its pages (params: ["Chapter's URL"])
+    //
+    // STATUS code meaning:
+    // - 200 -> This means "Ok"
+    // - 400 -> This means "The format is wrong, WTF is wrong with you!?"
+    // - 404 -> This means "No data"
+    // - 444 -> This means "Network problem"
+
+    use super::*;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Request {
+        pub command: Command,
+        pub version: String,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Response<T> {
+        pub status: Status,
+        pub reason: String,
+        pub source_name: String,
+        pub content: T,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    #[serde(tag = "command", content = "args")]
+    pub enum Command {
+        Ping,
+        Search {
+            keyword: String,
+            page: u32,
+            filter: Filter,
+        },
+        Chapters {
+            identifier: String,
+            page: u32,
+            filter: Filter,
+        },
+        Pages {
+            identifier: String,
+        },
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub enum Status {
+        Ok,
+        Error,
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
